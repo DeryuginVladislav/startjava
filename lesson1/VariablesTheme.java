656d048e8e21bd0c0f860dd096335acf25f50acd
DeryuginVladislav <vladderugin4@gmail.com>
2022-11-25 15:49:03 +0300

    Исправил ошибки

--- lesson1/VariablesTheme.java
+++ lesson1/VariablesTheme.java
@@ -113,7 +113,7 @@ public class VariablesTheme {
         int seconds = 86399;
         int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
-        seconds = seconds % 60;
+        seconds %= 60;
         System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
