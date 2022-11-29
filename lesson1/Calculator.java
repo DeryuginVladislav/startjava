305f37fc503741ef4e5eb164dab3e2b7623bb95b
DeryuginVladislav <vladderugin4@gmail.com>
2022-11-29 13:24:27 +0300

    Итоговое дз

---
+++ lesson1/Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 3;
+        char action = '^';
+        double result = 0.0;
+        switch (action) {
+            case ('+'):
+                result = num1 + num2;
+                break;
+            case ('-'):
+                result = num1 - num2;
+                break;
+            case ('*'):
+                result = num1 * num2;
+                break;
+            case ('/'):
+                result = (double) num1 / (double) num2;
+                break;
+            case ('%'):
+                result = num1 % num2;
+                break;
+            case ('^'):
+                result = num1;
+                for (int i = 1; i < num2; i++) {
+                    result *= num1;
+                }
+                break;
+        }
+        System.out.println(num1 + " " + action + " " + num2 + " = " + result);
+    }
+}
